d6abf9058e4c313fdb863c39868cf73c3d28c751
Nicholas Lui <nicholasluimy@gmail.com>
2015-05-29 11:14:10 +0800

    Add-row button functionality

---
+++ js/rowbuttons.js
@@ -0,0 +1,19 @@
+$(function(){
+	$('#addrow').click(function(){
+		$('#rowcontainer').append(
+          +'<input id="icon_prefix" type="text" class="validate"><label for="icon_prefix"><a class="tooltipped"'
+          +'data-position="bottom" data-delay="50" data-tooltip="Enter name">Name</a></label></div><div class="input-field col m6">'
+          +'<i class="small mdi-editor-format-underline prefix"></i><input id="icon_prefix" type="text" class="validate">'
+          +'<label for="icon_prefix"><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Input NUSmods timetable URL">Timetable URL</a></label>'
+          +'</div><!-- one br for first, two for subsequent... experiment --><div class="col m3"><br><div class="switch"><label>'
+          +'Hidden<input type="checkbox" onclick="Materialize.toast("Hidden/Visible toggled", 1000, "rounded")"><span class="lever"></span>'
+          +'Visible</label></div></div></div>');
+
+	});
+});
+
+
+
+
+
+
