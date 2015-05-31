50f5c0f726bd5fede2a0bc217cb004ef0ee89b6a
Nicholas Lui <nicholasluimy@gmail.com>
2015-05-31 22:38:16 +0800

    add-remove button logic
    change to IDs of rows added/removed for further manipulation of sections

--- js/rowbuttons.js
+++ js/rowbuttons.js
@@ -1,12 +1,16 @@
+var numrow = 1;
+
 $(function(){
 	$('#addrow').click(function(){
+		numrow += 1;
+		
 		$('#rowcontainer').append(
-			'<div class="row"><div class="input-field col m3"><i class="small mdi-action-account-circle prefix"></i>'
-          +'<input id="icon_prefix" type="text" class="validate"><label for="icon_prefix"><a class="tooltipped"'
+			'<div id="row'+numrow+'" class="row"><div class="input-field col m3"><i class="small mdi-action-account-circle prefix"></i>'
+          +'<input id="nameinput'+numrow+'" type="text" class="validate"><label for="nameinput'+numrow+'"><a class="tooltipped"'
           +'data-position="bottom" data-delay="50" data-tooltip="Enter name">Name</a></label></div><div class="input-field col m6">'
-          +'<i class="small mdi-editor-format-underline prefix"></i><input id="icon_prefix" type="text" class="validate">'
-          +'<label for="icon_prefix"><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Input NUSmods timetable URL">Timetable URL</a></label>'
-          +'</div><!-- one br for first, two for subsequent... experiment --><div class="col m3"><br><div class="switch"><label>'
+          +'<i class="small mdi-editor-format-underline prefix"></i><input id="urlinput'+numrow+'" type="text" class="validate">'
+          +'<label for="urlinput'+numrow+'"><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Input NUSmods timetable URL">Timetable URL</a></label>'
+          +'</div><div class="col m3"><br><div id="switch'+numrow+'" class="switch"><label>'
           +'Hidden<input type="checkbox" onclick="Materialize.toast("Hidden/Visible toggled", 1000, "rounded")"><span class="lever"></span>'
           +'Visible</label></div></div></div>');
 
@@ -14,7 +18,15 @@ $(function(){
 
 
 	$('#removerow').click(function(){
-		$('#rowcontainer').find('.row:last').remove();
+
+		if (numrow == 1){
+			return;
+		}
+		else{
+			numrow -= 1;
+			$('#rowcontainer').find('.row:last').remove();
+		}
+		
 
 	});
 
